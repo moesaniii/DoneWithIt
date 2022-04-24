8ccdcd3c4cedb1763af61e9631704cf2d4edd7d9
moesaniii <mohammad.mes1373@gmail.com>
2022-04-25 01:56:49 +0430

    ListingScreen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import AccountScreen from "./app/screens/AccountScreen";
+import ListingScreen from "./app/screens/ListingScreen";
 export default function App() {
-  return <AccountScreen />;
+  return <ListingScreen />;
 }
