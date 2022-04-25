be9fe6cf935953433143ee311803906f124c3ee6
moesaniii <mohammad.mes1373@gmail.com>
2022-04-26 03:22:42 +0430

    piciker

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
-import { Text, TextInput } from "react-native";
+
+import AppPicker from "./app/components/AppPicker";
 import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
 
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
+
 export default function App() {
-  const [firstName, setFirstName] = useState("");
+  const [category, setCategory] = useState();
+
   return (
     <Screen>
-      <AppTextInput placeholder="Username" icon="email" />
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon={"apps"}
+        placeholder={"Category"}
+      />
+      <AppTextInput icon={"email"} placeholder="Email" />
     </Screen>
   );
 }
